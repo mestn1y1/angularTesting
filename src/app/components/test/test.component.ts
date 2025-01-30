8aeeff5164dba20365adc14f94af41dbbabd1e0c
Serhii Tyron <mestn1y@hotmail.com>
2025-01-31 00:06:18 +0200

    use and test ngForms,eventEmmiter,doublebounding

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss',
 })
@@ -18,11 +18,24 @@ export class TestComponent {
   isDisabled: boolean = true;
   isClickedState: boolean = false;
 
+  inputText: string = '';
+
+  // -0-0--0-0-0-0-0-0-00--00--0-0-0-00--00-0--0-0-0-0-0-0-0-0-0-00--0-0
+
+  @Input() childMessage: string = '';
+
+  @Output() messageFromChild = new EventEmitter<string>();
+  // -0-0--0-0-0-0-0-0-00--00--0-0-0-00--00-0--0-0-0-0-0-0-0-0-0-00--0-0
+
   getFullName() {
     return `My name is ${this.firstName} ${this.lastName}`;
   }
   toggleState() {
     this.isClickedState = !this.isClickedState;
     console.log(this.isClickedState);
+  }
+
+  sendMessageToParent() {
+    this.messageFromChild.emit('I am your child');
   }
 }
